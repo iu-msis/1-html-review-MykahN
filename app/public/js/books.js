19d2851383ba0c0a9b4199ea3d89139d6eb07757
MykahN <89401136+MykahN@users.noreply.github.com>
2021-10-20 23:21:45 -0400

    Added posting new book functionality

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -1,33 +1,57 @@
 const BookApp = {
-    data() {
-      return {
-          "books": {
-            title: {},
-            author: {},
-            year_published: {},
-            publisher: {},
-            page_count: {},
-            msrp: {},
-          },
-      }
-    },
-    computed: {},
-    methods: {
-        fetchBooksData() {
-            fetch('/api/books/')
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                console.log(responseJson);
-                this.books = responseJson;
-            })
-            .catch( (err) => {
-                console.error(err);
-            })
-        },
-    }, 
-    created() {
-        this.fetchBooksData();
+  data() {
+    return {
+        books: [],
+        bookForm: {},
+      "books":  {
+          title: {},
+          author: {},
+          year_published: {},
+          publisher: {},
+          page_count: {},
+          msrp: {},
+      },
     }
-  }
+  },
+  computed: {},
+  methods: {
+      fetchBooksData() {
+          fetch('/api/books/')
+          .then(response => response.json())
+          .then( (responseJson) => {
+              console.log(responseJson);
+              this.books = responseJson;
+          })
+          .catch( (err) => {
+              console.error(err);
+          })
+      },
+       postNewBook(evt) {
+                 
+          console.log("Posting:", this.bookForm);
+          // alert("Posting!");
   
-  Vue.createApp(BookApp).mount('#booksApp');
+          fetch('api/books/create.php', {
+              method:'POST',
+              body: JSON.stringify(this.bookForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.books = json;
+              
+              // reset the form
+              this.bookForm = {};
+            });
+      }
+  },
+  created() {
+      this.fetchBooksData();
+  }
+}
+
+Vue.createApp(BookApp).mount('#booksApp'); 
